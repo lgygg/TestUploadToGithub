46ef58870d9c0421b939643da3e60e6e6902d7b2
LGY <liangguiyugg@163.com>
2017-07-13 15:18:06 +0800

    test commit

---
+++ app/src/main/java/com/tan/lgy/testuploadtogithub/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tan.lgy.testuploadtogithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
